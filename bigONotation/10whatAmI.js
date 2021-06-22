8637063d392da79500471943c2734fdba6c69af3
Christine Yoo <christinecho.s@gmail.com>
2021-06-22 12:46:06 -0700

    completed exercise 10

--- bigONotation/10whatAmI.js
+++ bigONotation/10whatAmI.js
@@ -11,4 +11,4 @@ function isWhat(n) {
     return true;
 }
 
-// ANSWER: This algorithm tests if a number is prime. 
+// ANSWER: This algorithm tests if a number is prime. The Big O for this one is polynomial time O(n^k) because if n=2, it would take 1 time unit. But if n is doubled so if n=4, then it would take double the time because the if condition would be met, thereby creating another iteration.
