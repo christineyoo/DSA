99f3ab2a7f75e91d6ca2c9239dfe05e267fc5d6a
Christine Yoo <christinecho.s@gmail.com>
2021-06-28 16:05:40 -0700

    added hashmap class

---
+++ HashMap_drills.js
@@ -0,0 +1,22 @@
+const HashMap = require('./hashMaps/hashMap');
+
+function main() {
+  const lotr = new HashMap();
+  const MAX_LOAD_RATIO = 0.5;
+  const SIZE_RATIO = 3;
+
+  lotr.set('Hobbit', 'Bilbo');
+  lotr.set('Hobbit', 'Frodo');
+  lotr.set('Wizard', 'Gandalf');
+  lotr.set('Human', 'Aragorn');
+  lotr.set('Elf', 'Legolas');
+  lotr.set('Maiar', 'The Necromancer');
+  lotr.set('Maiar', 'Sauron');
+  lotr.set('RingBearer', 'Gollum');
+  lotr.set('LadyOfLight', 'Galadriel');
+  lotr.set('HalfElven', 'Arwen');
+  lotr.set('Ent', 'Treebeard');
+  lotr.display();
+}
+
+main();
