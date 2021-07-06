c4110b084e3a386104b222635432f62488ab647b
Christine Yoo <christinecho.s@gmail.com>
2021-07-06 10:54:47 -0700

    implemented palindromeCount function

--- practice.js
+++ practice.js
@@ -50,17 +50,31 @@ function palindrome(str) {
   const re = /[^A-Za-z0-9]/g;
   str = str.toLowerCase().replace(re, '');
   const len = str.length;
-  for (let i = 0; i < len/2; i++) {
+  if (str.length === 1) return false;
+  for (let i = 0; i < len / 2; i++) {
     if (str[i] !== str[len - 1 - i]) {
-        return false;
+      return false;
     }
   }
   return true;
- }
+}
+
+function palindromeCount(s) {
+  const words = s.split(' ');
+  let palArr = [];
+  words.forEach((word) => {
+    if (palindrome(word)) {
+      palArr.push(word);
+    } else {
+      palArr;
+    }
+  });
+  return `${palArr} ${palArr.length} palindromes`;
+}
 
 // console.log(deleteDuplicates([1, 3, 3, 6, 7, 2, 6]));
 
-console.log(palindrome('dds'));
+console.log(palindromeCount('Dad gave mom a Tesla as a racecar'));
 
 // console.log(wordOccurances('The the sky is blue, and my favorite color is blue!'));
 
