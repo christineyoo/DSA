3064e0ce250d4fb503a50561a228629bbb2e07c6
Christine Yoo <christinecho.s@gmail.com>
2021-07-05 22:27:48 -0700

    implemented palindrome function

--- practice.js
+++ practice.js
@@ -46,7 +46,21 @@ const deleteDuplicates = (arr) => {
   return newArr;
 };
 
-console.log(deleteDuplicates([1, 3, 3, 6, 7, 2, 6]));
+function palindrome(str) {
+  const re = /[^A-Za-z0-9]/g;
+  str = str.toLowerCase().replace(re, '');
+  const len = str.length;
+  for (let i = 0; i < len/2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+        return false;
+    }
+  }
+  return true;
+ }
+
+// console.log(deleteDuplicates([1, 3, 3, 6, 7, 2, 6]));
+
+console.log(palindrome('dds'));
 
 // console.log(wordOccurances('The the sky is blue, and my favorite color is blue!'));
 
