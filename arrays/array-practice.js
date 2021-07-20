0199f33e71ae8eaf82f21d8485e155df53020f72
Christine Yoo <christinecho.s@gmail.com>
2021-07-19 22:14:31 -0700

    added array practice

---
+++ arrays/array-practice.js
@@ -0,0 +1,73 @@
+const findMax = (num1, num2) => {
+  return Math.max(num1, num2);
+};
+
+// don't explicitly return true or false in a ternary
+const isLandscape = (width, height) => {
+  return width > height;
+};
+
+const fizzBuzz = (num) => {
+  if (typeof num !== 'number') return NaN;
+  else if (num % 15 === 0) return 'FizzBuzz';
+  else if (num % 3 === 0) return 'Fizz';
+  else if (num % 5 === 0) return 'Buzz';
+  return num;
+};
+
+const demeritPoints = (speed) => {
+  const speedLimit = 70;
+  const kmPerPoint = 5;
+  if (speed <= speedLimit + kmPerPoint) return 'Ok';
+  else {
+    const points = Math.floor((speed - speedLimit) / kmPerPoint);
+    if (points >= 12) return 'License suspended';
+    else return `${points} points`;
+  }
+};
+
+const showNumbers = (limit) => {
+  for (let i = 0; i <= limit; i++) {
+    const message = i % 2 === 0 ? 'EVEN' : 'ODD';
+    console.log(i, message);
+  }
+};
+
+const countTruthy = (array) => {
+  let count = 0;
+  for (const item of array) {
+    if (item) count++;
+  }
+  return count;
+};
+
+const showProperties = (object) => {
+  for (let key in object) {
+    if (typeof object[key] === 'string') console.log(key, object[key]);
+  }
+};
+
+//counting repeating values
+const sum = (limit) => {
+  let multiples = [];
+  for (let i = 3; i <= limit; i += 3) {
+    multiples.push(i);
+  }
+  for (let i = 5; i <= limit; i += 5) {
+    multiples.push(i);
+  }
+  return multiples.reduce((a, b) => a + b, 0);
+};
+
+//not counting repeating values
+const sumWithoutRepeats = (limit) => {
+  let sum = 0;
+  for (let i = 0; i <= limit; i++) {
+    if (i % 3 === 0 || i % 5 === 0) sum += i;
+  }
+  return sum;
+};
+
+console.log(sum(18));
+//3, 6, 9, 12, 15 18
+//5, 10, 15
