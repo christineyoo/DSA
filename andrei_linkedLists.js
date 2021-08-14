7f16bf17b12ccce7507ff09147188b128172194a
Christine Yoo <christinecho.s@gmail.com>
2021-08-14 10:05:57 -0700

    implemented insertAT

--- andrei_linkedLists.js
+++ andrei_linkedLists.js
@@ -30,10 +30,39 @@ class LinkedList {
         this.length++;
         return this;
     }
+
+    printList() {
+        const array = [];
+        let currNode = this.head;
+        while (currNode !== null) {
+            array.push(currNode.value);
+            currNode = currNode.next;
+        }
+        return array;
+    }
+
+    insertAt(value, index) {
+        if (index > this.length) return;
+        if (this.length === 0) return this.append(value);
+        
+        const newNode = new Node(value);
+        let currNode = this.head;
+        let prev;
+        let count = 0;
+        while (currNode !== null && count < index) {
+            prev = currNode;
+            currNode = currNode.next;
+            count++;
+        }
+        prev.next = newNode;
+        newNode.next = currNode;
+        this.length++;
+    }
 }
 
 const ll = new LinkedList(10);
 ll.append(5);
 ll.append(16);
 ll.prepend(1);
-console.log(ll);
+ll.insertAt(15, 2);
+console.log(ll.printList());
