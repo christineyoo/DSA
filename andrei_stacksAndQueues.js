5eb39e807ba65806ee380a0e69c4c66eb413c37d
Christine Yoo <christinecho.s@gmail.com>
2021-09-09 10:40:55 -0700

    implemented peek, push, pop on stack

--- andrei_stacksAndQueues.js
+++ andrei_stacksAndQueues.js
@@ -1,49 +1,70 @@
 //you can build a stack with array or linked list
-//major thing about arrays is that array
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// ;lkj;lkj;lkj;lkj
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+    
+    // look at the top node
+    peek() {
+        console.log('Peekaboo', this.top);
+    }
+
+    //add a node to the top of the stack
+    push(value) {
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.bottom = newNode;
+            this.top = newNode;
+        } else {
+            let currNode = this.bottom;
+            let count = 1;
+            while (count < this.length) {
+                currNode = currNode.next;
+                count++;
+            }
+            currNode.next = newNode;
+            this.top = newNode;
+        }
+        this.length++;
+        // console.log(this);
+    }
+
+    //remove a node from the top
+    pop() {
+        let removedNode = this.top;
+
+        if (this.length <= 0) return;
+        if (this.length === 1) {
+            this.bottom = null;
+        } else {
+            let currNode = this.bottom;
+            let count = 1;
+            while (count < this.length - 1) {
+                currNode = currNode.next;
+                count++;
+            }
+            currNode.next = removedNode.next;
+            this.top = currNode;
+        }
+        this.length--;
+        // console.log(this);
+    }
+}
+
+const myStack =  new Stack();
+myStack.push('google');
+myStack.push('udemy');
+myStack.push('discord');
+console.log('before pop', myStack)
+myStack.pop();
+myStack.peek();
---
+++ javascript.js
@@ -0,0 +1,5 @@
+// ["other", "this", "thing", "that"]
+// ["thing", "other", "that", "this"]
+
+// f1(a1, a2) -> shuffle
+// f2(a1, shuffle) -> a2 (basically the inverse of f1)
