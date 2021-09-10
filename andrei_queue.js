1fcf87cafebd04402a6350d902206d29bc6e2fcd
Christine Yoo <christinecho.s@gmail.com>
2021-09-10 12:55:50 -0700

    implemented better dequeue algorithm

--- andrei_queue.js
+++ andrei_queue.js
@@ -1,46 +1,49 @@
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class Queue {
-    constructor() {
-        this.first = null;
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.first;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  dequeue() {
+    if (!this.first) {
+        return null;
+    }
+    if (this.first === this.last) {
         this.last = null;
-        this.length = 0;
     }
+    this.first = this.first.next;
+    this.length--;
+    return this;
+  }
 
-    peek() {
-        return this.first;
-    }
-
-    enqueue(value) {
-        const newNode = new Node(value);
-        if (this.length === 0) {
-            this.first = newNode;
-            this.last = newNode;
-        } else {
-            let currNode = this.first;
-            let count = 1;
-            while (count < this.length) {
-                currNode = currNode.next;
-                count++;
-            }
-            currNode.next = newNode;
-            this.last = newNode;
-        }
-        this.length++;
-        return this;
-    }
-
-    dequeue() {
-        let currHead = this.first;
-        this.first = currHead.next;
-        this.length--;
-        return this;
-    }
+  isEmpty() {
+    return this.length === 0;
+  }
 }
 
 const myQueue = new Queue();
@@ -49,5 +52,11 @@ myQueue.enqueue('priscilla');
 myQueue.enqueue('samir');
 myQueue.enqueue('jorge');
 // console.log(myQueue.peek());
-myQueue.dequeue();
-console.log(myQueue);
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+
+console.log(myQueue.dequeue());
+
+// console.log(myQueue);
+// console.log('isEmpty', myQueue.isEmpty());
