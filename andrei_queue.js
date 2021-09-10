30093f2d9a922079ae7cc016b7c6e581fb971e50
Christine Yoo <christinecho.s@gmail.com>
2021-09-10 13:22:14 -0700

    implemented stack using queue

--- andrei_queue.js
+++ andrei_queue.js
@@ -12,10 +12,12 @@ class Queue {
     this.length = 0;
   }
 
+//O(1) time | O(1) space
   peek() {
     return this.first;
   }
 
+//O(1) time | O(1) space
   enqueue(value) {
     const newNode = new Node(value);
     if (this.length === 0) {
@@ -29,6 +31,7 @@ class Queue {
     return this;
   }
 
+//O(1) time | O(1) space
   dequeue() {
     if (!this.first) {
         return null;
@@ -52,11 +55,54 @@ myQueue.enqueue('priscilla');
 myQueue.enqueue('samir');
 myQueue.enqueue('jorge');
 // console.log(myQueue.peek());
-console.log(myQueue.dequeue());
-console.log(myQueue.dequeue());
-console.log(myQueue.dequeue());
-
-console.log(myQueue.dequeue());
-
+// console.log(myQueue.dequeue());
+// console.log(myQueue.dequeue());
+// console.log(myQueue.dequeue());
+// console.log(myQueue.dequeue());
 // console.log(myQueue);
 // console.log('isEmpty', myQueue.isEmpty());
+
+// IMPLEMENT A QUEUE USING A STACK
+class CrazyQueue {
+    constructor() {
+        this.first = [];
+        this.last = [];
+    }
+
+    enqueue(value) {
+        const length = this.first.length;
+        for (let i = 0; i < length; i++) {
+            this.last.push(this.first.pop());
+        }
+        this.last.push(value); //adds to the end of the queue
+        return this;
+    }
+
+    dequeue() {
+        const length = this.last.length;
+        for (let i = 0; i < length; i++) {
+            this.first.push(this.last.pop());
+        }
+        this.first.pop(); //removes and returns the first one that went in
+        return this;
+    }
+
+    peek() {
+        if (this.last.length === 0) {
+            return this.first[this.first.length - 1];
+        } else if (this.last.length > 0) {
+            return this.last[0];
+        }
+    }
+}
+
+const myCrazyQueue = new CrazyQueue();
+myCrazyQueue.enqueue('a');
+myCrazyQueue.enqueue('b');
+myCrazyQueue.enqueue('c');
+myCrazyQueue.enqueue('d');
+// myCrazyQueue.dequeue();
+console.log(myCrazyQueue.peek());
+
+
+console.log(myCrazyQueue);
