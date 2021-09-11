1fc41828045c90cba933b096a45e78cf4019a193
Christine Yoo <christinecho.s@gmail.com>
2021-09-11 10:00:50 -0700

    implemented iterative insert method

--- andrei_bst.js
+++ andrei_bst.js
@@ -9,7 +9,46 @@ class Node {
 class BinarySearchTree {
     constructor() {
         this.root = null;
-        this.left = null;
-        this.right = null;
+    }
+
+    insert(value) {
+       const newNode = new Node(value);
+       if (this.root === null) {
+           this.root = newNode;
+       } else {
+            let currentNode = this.root;
+            while (true) {
+               if (value < currentNode.value) {
+                   if (!currentNode.left) {
+                       currentNode.left = newNode;
+                       return this;
+                   }
+                   currentNode = currentNode.left; //update the node to the left child
+               } else {
+                   if (!currentNode.right) {
+                       currentNode.right = newNode;
+                       return this;
+                   }
+                   currentNode = currentNode.right;
+               }
+           }
+       }
     }
 }
+
+const traverse = (node) => {
+    const tree = {value: node.value}
+    tree.left = node.left === null ? null : traverse(node.left);
+    tree.right = node.right === null ? null : traverse(node.right);
+    return tree;
+}
+
+const bst = new BinarySearchTree();
+bst.insert(9);
+bst.insert(4);
+bst.insert(6);
+bst.insert(20);
+bst.insert(170);
+bst.insert(15);
+bst.insert(1);
+console.log(JSON.stringify(traverse(bst.root)));
--- javascript.js
+++ javascript.js
@@ -3,3 +3,5 @@
 
 // f1(a1, a2) -> shuffle
 // f2(a1, shuffle) -> a2 (basically the inverse of f1)
+
+//Create a tic-tac-toe class with a .move method. Do this in O(1) time
