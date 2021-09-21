e8e6afd53d47c0774e4b2af461385f422b0bf02e
Christine Yoo <christinecho.s@gmail.com>
2021-09-20 18:18:33 -0700

    implemented insertion sort

--- andrei_sorting.js
+++ andrei_sorting.js
@@ -30,8 +30,25 @@ function selectionSort(array) {
     }
   }
   return array;
+  
+}
+
+function insertionSort(array) {
+    const length = array.length;
+    for (let i = 0; i < length; i++) {
+        if (array[i] < array[0]) {
+            array.unshift(array.splice(i, 1)[0]); // move number to the first position
+        } else {
+            for (let j = 1; j < i; j++) {
+                if (array[i] > array[j - 1] && array[i] < array[j]) {
+                    array.splice(j, 0, array.splice(i, 1)[0]);    // move number to the right spot
+                }
+            }
+        }
+    }
+    return array;
 }
 
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-console.log(selectionSort(numbers));
+console.log(insertionSort(numbers));
