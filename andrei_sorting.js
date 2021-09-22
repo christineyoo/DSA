cb95f708c0fd75430908bd30a2a0e245a96f4224
Christine Yoo <christinecho.s@gmail.com>
2021-09-21 20:34:03 -0700

    added merge sort and quick sort

--- andrei_sorting.js
+++ andrei_sorting.js
@@ -49,6 +49,99 @@ function insertionSort(array) {
     return array;
 }
 
+function mergeSort(array) { //this function splits the array down to half each time
+  if (array.length === 1) {
+    return array;
+  }
+
+  const length = array.length;
+  const middle = Math.floor(length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) { //takes the left and right element and compares them and places the elements in the correct position
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+function quickSort(array, left, right){
+  const len = array.length; 
+  let pivot;
+  let partitionIndex;
+
+  if(left < right) {
+    pivot = right;
+    partitionIndex = partition(array, pivot, left, right);
+    
+    //sort left and right
+    quickSort(array, left, partitionIndex - 1);
+    quickSort(array, partitionIndex + 1, right);
+  }
+  return array;
+}
+   
+function partition(array, pivot, left, right){
+  let pivotValue = array[pivot];
+  let partitionIndex = left;
+
+  for(let i = left; i < right; i++) {
+    if(array[i] < pivotValue){
+      swap(array, i, partitionIndex);
+      partitionIndex++;
+    }
+  }
+  swap(array, right, partitionIndex);
+  return partitionIndex;
+}
+
+function swap(array, firstIndex, secondIndex){
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+}
+
+
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-console.log(insertionSort(numbers));
+//Select first and last index as 2nd and 3rd parameters
+quickSort(numbers, 0, numbers.length - 1);
+console.log(numbers);
+
+//SORT TYPES
+// Bubble Sort
+// Selection Sort
+// Insertion Sort
+// Merge Sort
+// Quick Sort
+
+//#1 - Sort 10 schools around your house by distance: insertion sort - it's the simplest for this type of data
+
+//#2 - eBay sorts listings by the current Bid amount: Radix or counting sort because you're dealing with numbers from 1 to <50,000
+
+//#3 - Sport scores on ESPN: quick sort - it's the most efficient :)
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data: Merge sort because it's already not fitting into the memory, so you want to optimize the time
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews: insertion sort - becasue i'm assuming that most of the data is already sorted
+
+//#6 - Temperature Records for the past 50 years in Canada: radix sort or counting sort because you're working with small numbers
+
+//#7 - Large user name database needs to be sorted. Data is very random: quick sort bc it's space efficient
+
+//#8 - You want to teach sorting for the first time: bubble sort, selection sort, insertion sort
+
